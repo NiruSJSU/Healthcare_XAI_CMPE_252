04a12993bedf44a6c2fbd4a28ced5901e5d9a53c
neharao24 <nehavrao2016@gmail.com>
2026-04-25 20:29:02 -0700

    removed commented out code_2

--- src/models/train.py
+++ src/models/train.py
@@ -17,7 +17,6 @@ def train_and_verify(model, X, y, name, ax):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
     
     # Verification (5 fold)
-    # cv_scores = cross_val_score(model, X_train, y_train, cv=5, scoring='accuracy')
     cross_val_score(model, X_train, y_train, cv=5, scoring='accuracy')
     
     print(f"\n---- {name} ----")
