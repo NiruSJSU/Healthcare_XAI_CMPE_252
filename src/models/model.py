8400257a8dba6f4d5b699f6362d574640775b6d8
Nirupam Narayana Maladi <ramakrishnamaladi@Nirus-MacBook-Pro.local>
2026-05-01 15:46:56 -0700

    Fixed minor bug in models

--- src/models/model.py
+++ src/models/model.py
@@ -8,7 +8,7 @@ def get_model_pipeline():
     models = [
         (LogisticRegression(max_iter=1000, class_weight='balanced'), "Logistic Regression"),
         (RandomForestClassifier(random_state=42, class_weight='balanced'), "Random Forest"),
-        (SVC(probability=True, random_state=42, class_weight='balanced'), "SVM")
+        (SVC(probability=True, random_state=42, class_weight='balanced'), "SVM"),
         (MLPClassifier(hidden_layer_sizes=(16, 8), max_iter=1000, random_state=42), "MLP"),
         (LinearDiscriminantAnalysis(), "LDA")
     ]
